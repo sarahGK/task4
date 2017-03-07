bdf6b36852b33b2b6c4f678f119ec5529bbadc76
sarahGK <xin.kelly@hotmail.com>
2017-03-07 11:39:06 -0800

    Update data.h
    Add comments to the code

--- data.h
+++ data.h
@@ -1,26 +1,25 @@
+// 1296 classes for phi/psi by dividing X-axis and Y-axis of Ramachandran plot by 10 degree(36*36)
 #define LA 1296
 const char rname_one_rank20[21]="ACDEFGHIKLMNPQRSTVWY";
-
+// define a class for all the conditional probabilities
+// It's for entropy evaluation and probability term for the weighted function
 class data{
 private:
-	double sa[LA][20];//p(si|ai-1)
-	double sb[LA][20];//p(si|ai)
-	double sc[LA][20];//p(si|ai+1)
-	//double sd[LA][LA];//p(si|si-1)
-	//double se[LA][LA];//p(si|si+1)
+	double sa[LA][20];	//p(si|ai-1)
+	double sb[LA][20];	//p(si|ai)
+	double sc[LA][20];	//p(si|ai+1)
 	vector<vector<double>> sd;//p(si|si-1)
 	vector<vector<double>> se;//p(si|si+1)
-	double sf[LA][3];//p(si|chi-1)
-	double sg[LA][3];//p(si|chi)
-	double sh[LA][3];//p(si|chi+1)
-	bool mark[8];//false为条件不可取，true为可取
+	double sf[LA][3];	//p(si|chi-1)
+	double sg[LA][3];	//p(si|chi)
+	double sh[LA][3];	//p(si|chi+1)
+	bool mark[8];		// True if the according condition taken, otherwise false
 public:
 	data();
-	void set_data(int s,int a,int b,int c,int d,int e, int f, int g, int h);//统计出现的个数
-	//void set_data(int s, int f, int g, int h);
-	void set_data(double **dataarray,int length,int no);//计算概率值
-	void set_data(vector<vector<double>> & de,int no);//计算概率值
-	void readfile(ifstream &f1,ifstream &f2);//文件中读取数据
-	void print_data(ostream & of);//输出mark
+	void set_data(int s,int a,int b,int c,int d,int e, int f, int g, int h);//count
+	void set_data(double **dataarray,int length,int no);			//calculate probabilistic value for 2D array
+	void set_data(vector<vector<double>> & de,int no);			//calculate probabilistic value for vector
+	void readfile(ifstream &f1,ifstream &f2);				//read data
+	void print_data(ostream & of);						//print the value of mark
 
 };
